38f6c9ba52bf55bcb106fffbbe60c4b86f8b68c2
Harshit Gupta <harshithigh2001@gmail.com>
2023-08-05 00:02:09 +0530

    Revert "added model and controller files"
    This reverts commit 1ceb1c1637dfc789b8fcc75072af00641d8aef15.

--- src/main/java/com/example/bonds_backend/controller/SecurityController.java
+++
@@ -1,5 +0,0 @@
-package com.example.bonds_backend.controller;
-
-public class SecurityController {
-    
-}
--- src/main/java/com/example/bonds_backend/models/Book.java
+++
@@ -1,5 +0,0 @@
-package com.example.bonds_backend.models;
-
-public class Book {
-    
-}
