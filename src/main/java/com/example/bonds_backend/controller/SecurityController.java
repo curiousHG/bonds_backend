fdf3383eae92cd5867abc7df806e3b78e675cbf6
Harshit Gupta <harshithigh2001@gmail.com>
2023-08-05 14:07:45 +0530

    Added some getSecurity methods

--- src/main/java/com/example/bonds_backend/controller/SecurityController.java
+++ src/main/java/com/example/bonds_backend/controller/SecurityController.java
@@ -1,13 +1,37 @@
 package com.example.bonds_backend.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.example.bonds_backend.models.Security;
+import com.example.bonds_backend.repository.SecurityRepository;
 
+@RestController
+@RequestMapping("/api/v1")
 public class SecurityController {
     
+    @Autowired
+    private SecurityRepository securityRepository;
+
+    @GetMapping("/getAllSecurities")
+    public List<Security> getAll(){
+        return securityRepository.findAll();
+    }
+
+    @GetMapping("/getSecurityById/{id}")
+    public Security getById(@PathVariable long id){
+        return securityRepository.findById(id).orElse(null);
+    }
+
+    @GetMapping("/getSecurityByUserId/{userId}")
+    public List<Security> getByUserId(@PathVariable long userId){
+        return securityRepository.findByUserId(userId);
+    }
 }
--- src/main/java/com/example/bonds_backend/repository/SecurityRepository.java
+++ src/main/java/com/example/bonds_backend/repository/SecurityRepository.java
@@ -1,5 +1,22 @@
 package com.example.bonds_backend.repository;
 
-public class SecurityRepository {
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.example.bonds_backend.models.Security;
+import org.springframework.data.jpa.repository.Query;
+
+public interface SecurityRepository extends JpaRepository<Security, Long> {
+
     
+    @Query(value = "SELECT *" +
+        " FROM security s" +
+        " JOIN trade t ON s.id = t.security_id" +
+        " JOIN book_user bu ON t.book_id = bu.book_id" +
+        " JOIN users u ON bu.user_id = u.id" +
+        " WHERE u.id = ?1", nativeQuery = true)
+
+    List<Security> findByUserId(long userId);
+
 }
+
