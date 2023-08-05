2b513db1eae1443c0a01fa5586b81b35e138d66b
harshkumarchoudhary <harshkumarchoudhary144@gmail.com>
2023-08-05 11:38:51 +0530

    Completed user controller

--- src/main/java/com/example/bonds_backend/controller/UserController.java
+++ src/main/java/com/example/bonds_backend/controller/UserController.java
@@ -4,8 +4,11 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.example.bonds_backend.repository.UserRepository;
@@ -32,6 +35,30 @@ public class UserController {
     @GetMapping("/getAllUsers")
     public List<User> getAll(){
         return repository.findAll();
+    }
+
+    @GetMapping("/getUserById/{id}")
+    private User getById(@PathVariable long id){
+        return repository.findById(id).orElse(null);
+    }
+
+    @DeleteMapping("/deleteUser/{id}")
+    private String deleteUser(@PathVariable long id){
+        repository.deleteById(id);
+        return "Deleted user Successfully";
+    }
+
+    @PutMapping("/updateUser/{id}")
+    private User updateUser(@PathVariable long id, @RequestBody User updatedUser){
+        User user = repository.findById(id).orElse(null);
+        if(user != null){
+            user.setName(updatedUser.getName());
+            user.setEmail(updatedUser.getEmail());
+            repository.save(user);
+            return user;
+        }else{
+            return null;
+        }
     }
 
     @PostMapping("/signin")
